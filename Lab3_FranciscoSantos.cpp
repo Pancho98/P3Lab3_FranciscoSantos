4ee8322ce7e1b605c6bc382cab2549baa34df588
Francisco Santos <francisco@localhost.localdomain>
2018-02-09 13:34:30 -0600

    Menu y metodos de los ejercicios

---
+++ Lab3_FranciscoSantos.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<string>
+
+//
+
+//
+
+//
+
+
+
+using namespace std;
+int main(){
+	int respuesta=1;
+	while(respuesta!=4){
+		cout<<"----------Menu----------"<<endl;
+		cout<<"1. Ejercicio 1"<<endl;
+		cout<<"2. Ejercicio 2"<<endl;
+		cout<<"3. Ejercicio 3"<<endl;
+		cout<<"4. Salir"<<endl;
+		cin>>respuesta;
+
+		switch(respuesta){
+			case 1:{
+
+				   }
+				   break;
+			case 2:{
+
+				   }
+				   break;
+			case 3:{
+
+				   }
+				   break;
+		}
+	}
+
+	return 0;
+}
